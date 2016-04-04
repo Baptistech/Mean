9bd7f8ce688b77e3edbe619cb7692574a0631adf
foad <foad.ardalan@outlook.fr>
2016-04-05 01:11:17 +0200

    reorganisation

--- 1.node/2.scope/index.js
+++
@@ -1,7 +0,0 @@
-var mod1 = require('./mod1');
-console.log(one);
-//console.log(two());
-console.log(three);
-console.log(four());
-console.log(mod1.five);
-console.log(mod1.six());
--- 1.node/2.scope/mod1.js
+++
@@ -1,6 +0,0 @@
-var v1 = "v1";
-function f2 () { return "f2";}
-global.three = "vg3";
-global.four = function () { return "fg4"; };
-exports.five = "ef5";
-exports.six = function () { return "ef6"; };
