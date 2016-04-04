2a9168a0f4b5f447353fef919bce2ceaf9f4f1f4
foad <foad.ardalan@outlook.fr>
2016-04-05 00:55:13 +0200

    reorganisation

--- 2.express/7.blog/docker-run.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-docker run -d -v $(pwd):/app -w /app -p 27017 --name mongo mongo bash -c "mongod & cd db && ./seed.sh && sleep infinity"
-docker run -d --link mongo:mongo -v $(pwd):/app -w /app -p 3000:3000 node:5.4 node app
